0e1653c697e53ced7081022fd03cc857fff0d4d2
aquint714 <aquint@greenhillsschool.org>
2020-09-23 10:10:56 -0400

    I finished the questions. Could not figure out how to change numbers to names without if, else statements.

--- src/main/java/com/example/project/CalendarTester.java
+++ src/main/java/com/example/project/CalendarTester.java
@@ -10,7 +10,6 @@ public class CalendarTester {
         //This constructs a GregorianCalendar object
         GregorianCalendar myCal = new GregorianCalendar();
 
-
         /* You can use constants with the GregorianCalendar class to refer to its various
          * variables.  For example, "Calendar.MONTH" is a variable that refers to the
          * month of the calendar.  This can be used with the .get() method to test
@@ -18,22 +17,52 @@ public class CalendarTester {
          * */
 
         //Call the .get method on myCal using Calendar.DAY_OF_WEEK as the argument
-
-        System.out.println("myCal Day of Week: " + myCal.get(Calendar.DAY_OF_WEEK));
+        int weekday = myCal.get(Calendar.DAY_OF_WEEK);
+        System.out.println("myCal Day of Week: " + weekday );
         System.out.println("Expected: 4");     //Note: 1 = Sunday, 2 = Monday, etc.
 
-
         //Write similar tests for month, year and day of month.
+        int month = myCal.get(Calendar.MONTH) + 1;
+        System.out.println("myCal Month: " + month);
+        System.out.println("Expected: 9");
 
+        int monthday = myCal.get(Calendar.DAY_OF_MONTH);
+        System.out.println("myCal Day of Month: " + monthday);
+        System.out.println("Expected: 23");
+
+        int year = myCal.get(Calendar.YEAR);
+        System.out.println("myCal Year: " + year);
+        System.out.println("Expected: 2020");
+
+        System.out.println("Today's date is " + weekday + "/" + month + "/" + monthday + "/" + year);
 
         //Call the .add method to add 100 days to myCal.  Then, check the date and weekday of this new date.
         // To see that it is correct, check out this calendar calculator:
         //http://www.timeanddate.com/date/dateadd.html
 
+        myCal.add(Calendar.DAY_OF_YEAR,100);
+        int weekday2 = myCal.get(Calendar.DAY_OF_WEEK);
+        int month2 = myCal.get(Calendar.MONTH) + 1;
+        int monthday2 = myCal.get(Calendar.DAY_OF_MONTH);
+        int year2 = myCal.get(Calendar.YEAR);
+        System.out.println("The new date is " + weekday2 + "/" + month2 + "/" + monthday2 + "/" + year2);
+
+        myCal.add(Calendar.DAY_OF_YEAR,-171);
+        int weekday3 = myCal.get(Calendar.DAY_OF_WEEK);
+        int month3 = myCal.get(Calendar.MONTH) + 1;
+        int monthday3 = myCal.get(Calendar.DAY_OF_MONTH);
+        int year3 = myCal.get(Calendar.YEAR);
+        System.out.println("My birthday was " + weekday3 + "/" + month3 + "/" + monthday3 + "/" + year3);
 
         //Finally, find what day of the week is 10,000 days after your 2020 birthday. Note, you may have to play around
         //with adding or subtracting days to get to your birthday date (hmm, how can you subtract days?)
 
+        myCal.add(Calendar.DAY_OF_YEAR,10000);
+        int weekday4 = myCal.get(Calendar.DAY_OF_WEEK);
+        int month4 = myCal.get(Calendar.MONTH) + 1;
+        int monthday4 = myCal.get(Calendar.DAY_OF_MONTH);
+        int year4 = myCal.get(Calendar.YEAR);
+        System.out.println("The new date is " + weekday4 + "/" + month4 + "/" + monthday4 + "/" + year4);
 
     }
 
